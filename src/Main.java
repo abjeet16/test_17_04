7a38dde28f7b251c70296f0ada770af327ce795f
abjeet16 <avjeetyadav00@gmail.com>
2026-04-17 14:56:39 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+class Flower1{
+    public void display(){
+        System.out.println("this is the parent most flower");
+    }
+}
+class Flower2 extends Flower1{ }
+class Flower3 extends Flower2{ }
+public class Main {
+    public static void main(String[] args) {
+        Flower3 f = new Flower3();
+        f.display();
+    }
+}
